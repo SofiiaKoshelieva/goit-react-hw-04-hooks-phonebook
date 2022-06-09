4178848ea93905e5bfe7bfb78522922615b5fd75
SofiiaKoshelieva <sofikosh58@gmail.com>
2022-06-09 16:47:22 +0300

    hooks

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,48 +1,50 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import s from './Phonebook.module.css';
-class ContactForm extends Component {
-  state = { name: '', number: '' };
-  onInputChange = e => {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
-  };
-  submit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-  };
-  render() {
-    return (
-      <form onSubmit={this.submit}>
-        <fieldset className={s.fieldset}>
-          <label>
-            Name
-            <input
-              className={s.input}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              onChange={this.onInputChange}
-            />
-          </label>
-          <label>
-            Phone
-            <input
-              className={s.input}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              onChange={this.onInputChange}
-            />
-          </label>
-          <button className={s.button} type="submit">
-            Add contact
-          </button>
-        </fieldset>
-      </form>
-    );
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  function onNameChange(e) {
+    setName(e.currentTarget.value);
   }
+  function onNumberChange(e) {
+    setNumber(e.currentTarget.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(name, number);
+    e.target.reset();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset className={s.fieldset}>
+        <label>
+          Name
+          <input
+            className={s.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={onNameChange}
+          />
+        </label>
+        <label>
+          Phone
+          <input
+            className={s.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={onNumberChange}
+          />
+        </label>
+        <button className={s.button} type="submit">
+          Add contact
+        </button>
+      </fieldset>
+    </form>
+  );
 }
-export default ContactForm;
--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,79 +1,69 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
-
-class Phonebook extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidUpdate(prevProp, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts) {
-      this.setState(() => {
-        return { contacts };
-      });
-    }
-  }
-  onSubmit = data => {
-    const sameName = this.state.contacts.find(same => same.name === data.name);
-    this.setState(prevState => {
-      if (sameName) {
-        alert(`${data.name} is already in contacts.`);
-        return;
-      }
-      return {
-        contacts: [
-          ...prevState.contacts,
-          { name: data.name, id: nanoid(), number: data.number },
-        ],
-      };
-    });
-  };
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-  deleteContacts = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== contactId
-        ),
-      };
-    });
-  };
-
-  render() {
-    const normal = this.state.filter.toLowerCase();
-    const visibleContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normal)
-    );
-
+export default function Phonebook() {
+  const [contacts, setContacts] = useState(() => {
     return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.onSubmit} />
-        <h2>Contacts</h2>
-        <ContactList
-          contacts={visibleContacts}
-          deleteContacts={this.deleteContacts}
-        />
-        <Filter value={this.filter} onChange={this.changeFilter} />
-      </div>
+      JSON.parse(window.localStorage.getItem('contacts')) ?? [
+        {
+          id: 'id-1',
+          name: 'Rosie Simpson',
+          number: '459-12-56',
+        },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  function addContact(name, number) {
+    let isNameAlreadyExists = contacts.find(element => element.name === name);
+    if (isNameAlreadyExists) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    setContacts(prevState => {
+      return [
+        ...prevState,
+        {
+          name: name,
+          id: nanoid(),
+          number: number,
+        },
+      ];
+    });
+  }
+
+  function changeFilter(e) {
+    setFilter(e.currentTarget.value);
+  }
+
+  function filterContacts() {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   }
+
+  function deleteContact(contactId) {
+    setContacts(prevState => {
+      return prevState.filter(contact => contact.id !== contactId);
+    });
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <ContactList contacts={filterContacts()} deleteContacts={deleteContact} />
+      <Filter value={filter} onChange={changeFilter} />
+    </div>
+  );
 }
-export default Phonebook;
